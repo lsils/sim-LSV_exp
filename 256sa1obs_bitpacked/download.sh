fd75cfc4102bfd0514c7336fdacd9a1e00ef6580
Sonia_iccluster <siang-yun.lee@epfl.ch>
2021-04-07 00:51:37 +0200

    add download script

---
+++ 256sa1obs_bitpacked/download.sh
@@ -0,0 +1,26 @@
+# Use this script to download the large files from Google Drive
+
+fileid="1hF-5bqv7TwBxoV4QImbIi0-z_mBpVuRa"
+filename="leon2.pat"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+fileid="1qV3BWKubPloG7yufm65A_95Ul1qa98bg"
+filename="leon3.pat"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+fileid="1jjYBUR1AFH626XcS0XN1xNmt6ABBemE8"
+filename="leon3_opt.pat"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+fileid="14qGVfdyPgmoOH7CktKMrV2Mr1XkhfeeT"
+filename="leon3mp.pat"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+fileid="1NgACw69zlmJ03U5u539VEvg2_XObkg-x"
+filename="netcard.pat"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
